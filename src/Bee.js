d241d9fb179e64cc3bbd1850765fb29a8e6f7f7d
Imran Kheraj <ikheraj17@gmail.com>
2020-03-30 14:37:53 -0500

    Bee, Grub, and HoneyMakerBee are functioning properly

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
 var Bee = function() {
+  //call bee to inherit properties from grub
+  Grub.call(this);
+  //reassign age
+  this.age = 5;
+  //reassign color
+  this.color = "yellow";
+  this.job = "keep on growing";
 };
+
+
+
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
